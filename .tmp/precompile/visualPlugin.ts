21aae3544a147e99779b94fc62c191bd5dc7cab1
tsansome <travis.sansome@gmail.com>
2019-01-22 18:30:23 +0700

    had to repackage

--- .tmp/precompile/visualPlugin.ts
+++ .tmp/precompile/visualPlugin.ts
@@ -1,9 +1,9 @@
 module powerbi.visuals.plugins {
-    export var databarKPIB8060E2B144244C5A38807466893C9F6_DEBUG = {
-        name: 'databarKPIB8060E2B144244C5A38807466893C9F6_DEBUG',
+    export var databarKPIB8060E2B144244C5A38807466893C9F6 = {
+        name: 'databarKPIB8060E2B144244C5A38807466893C9F6',
         displayName: 'Data bar KPI',
         class: 'databarvisual',
-        version: '1.0.0',
+        version: '1.1.1',
         apiVersion: '1.7.0',
         create: (options: extensibility.visual.VisualConstructorOptions) => new powerbi.extensibility.visual.databarKPIB8060E2B144244C5A38807466893C9F6.databarvisual(options),
         custom: true
