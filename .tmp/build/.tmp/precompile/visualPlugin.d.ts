a448c75849bf59a3bb88cc6b22e346c2e3f7dc27
Travis Sansome <travis.sansome@dataaddiction.com.au>
2019-11-17 22:59:16 +1100

    Removing tracking of tmp

--- .tmp/build/.tmp/precompile/visualPlugin.d.ts
+++
@@ -1,4 +0,0 @@
-import powerbiVisualsApi from "powerbi-visuals-api";
-import IVisualPlugin = powerbiVisualsApi.visuals.plugins.IVisualPlugin;
-declare var databarKPIB8060E2B144244C5A38807466893C9F6: IVisualPlugin;
-export default databarKPIB8060E2B144244C5A38807466893C9F6;
